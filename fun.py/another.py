a10f9dce15c6c8b95bd661c7cb1ade131f3365ba
Abdullah979440 <kazi972263@gmail.com>
2026-08-16 20:16:21 +0600

    other practoce

---
+++ fun.py/another.py
@@ -0,0 +1,26 @@
+#remove danger words
+#words = input('enter sentense').split()
+#curse_words = ["darn", "dang", "freakin", "heck", "shoot"]
+#for i in range(len(words)):
+#    if words[i] in curse_words:
+#       words[i] = '*' * len(words[i])
+#print(words)
+
+#palindrom check using list comprehension 
+#palindrom = [num for num in range(100,1000) if str(num)== str(num)[::-1]]
+#print(palindrom)
+
+#zero_separation = [1] + [x for i in range (1,11) for x in [0]*i + [1]]
+#print(zero_separation)
+
+#L=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]
+#gaps = [L[i+1] - L[i] for i in range(len(L)-1)]
+#max_gaps = max(gaps)
+#percentage = gaps.count(2)/len(gaps)*100
+#
+#print(gaps)
+#print(max_gaps)
+#print(percentage)
+
+
+    
