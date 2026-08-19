edea026e57e1dd9de8e3e9436fb402e5c939859c
Abdullah979440 <kazi972263@gmail.com>
2026-08-19 20:01:17 +0600

    practice

--- fun.py/format.py
+++ fun.py/format.py
@@ -16,4 +16,13 @@ for i in range(3):
     final_price = prices[i] - discount
 
     print(f"{products[i]:<20} {prices[i]:>13.2f} {discount:>10.2f} {final_price:>13.2f}")'''
-    
+   
+'''count = 0
+for i in range(1,1001):
+    if '3' in str(i):
+        count +=1
+print(count)'''
+
+'''feet = float(input('enter your feet and inches'))
+inches =round((feet - int(feet))* 12)
+print(f'you {int(feet)} feet, {inches}inches')'''
